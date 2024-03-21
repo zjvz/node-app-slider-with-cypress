7933427a9ca71f31792e137a6a59ada3613bd1ff
zjvz <114100664+zjvz@users.noreply.github.com>
2024-03-21 19:18:03 +0100

    lab2

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,6 +1,6 @@
 describe('Swiper Gallery Test', function () {
   it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
   });
@@ -8,11 +8,40 @@ describe('Swiper Gallery Test', function () {
 
 describe('Swiper Gallery Test', function () {
   it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000)
     cy.get('.swiper-button-next').click({ force: true });
     cy.wait(2000)
     cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if zapetlenie', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000)
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if descriptions are correct', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.card-description').should('contain', 'Rome');
+    cy.get('.card-description').should('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000)
+    cy.get('.card-description').should('contain', 'London');
+    cy.get('.card-description').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.card-description').should('contain', 'Paris');
+    cy.get('.card-description').should('contain', 'France');
   });
 });
